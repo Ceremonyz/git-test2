5fb91e7534d20b20dd4abdc15fb8c07b6b06dd9c
wyz <1286153271@qq.com>
2022-07-06 10:51:56 +0800

    gitee commit

--- src/main/java/com/git/test.java
+++ src/main/java/com/git/test.java
@@ -9,6 +9,8 @@ public class test {
         System.out.println("hello233");
         System.out.println("hello233");
         System.out.println("hello233");
-        
+
+        System.out.println("gitee test");
+
     }
 }
